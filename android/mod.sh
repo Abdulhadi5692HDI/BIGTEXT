50c1e893ed67d929e0322cfe23bdb32203c76136
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2021-10-28 11:18:05 +0100

    Add files via upload

---
+++ android/mod.sh
@@ -0,0 +1,52 @@
+# Modded by <whoever is modding this>
+
+cyan="python cyan.py"
+$cyan
+echo "
+ ________  ___  ________ _________  _______      ___    ___ _________   
+|\   __  \|\  \|\   ____\\___   ___\\  ___ \    |\  \  /  /|\___   ___\ 
+\ \  \|\ /\ \  \ \  \___\|___ \  \_\ \   __/|   \ \  \/  / ||___ \  \_| 
+ \ \   __  \ \  \ \  \  ___  \ \  \ \ \  \_|/__  \ \    / /     \ \  \  
+  \ \  \|\  \ \  \ \  \|\  \  \ \  \ \ \  \_|\ \  /     \/       \ \  \ 
+   \ \_______\ \__\ \_______\  \ \__\ \ \_______\/  /\   \        \ \__\
+    \|_______|\|__|\|_______|   \|__|  \|_______/__/ /\ __\        \|__|
+                                                |__|/ \|__|             
+   
+"
+python cyan.py | echo "(MOD) [Example] No Updates right now"
+echo -e
+sleep 3
+python red.py
+echo ""
+echo "Installing Packages"
+# Add more packages here if needed so
+pip install colorama
+apt install figlet
+gem install lolcat
+$cyan
+echo "I| Now type text and press enter to get big ascii version"
+python red.py
+echo "Notice| You have 〰️ tries"
+$cyan
+# Do not,change this
+echo "text>>" | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text | lolcat
+read text
+figlet $text |lolcat
