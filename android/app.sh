555f19a24d74bd07fe9b9b3b00941515610162d8
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2021-11-15 20:08:33 +0000

    Update app.sh

--- android/app.sh
+++ android/app.sh
@@ -1,50 +1,40 @@
-
-# !!!Termux only!!!		
-cyan="python  /data/data/com.termux/files/usr/share/cyan.py"
-$cyan
-echo "
- ________  ___  ________ _________  _______      ___    ___ _________   
-|\   __  \|\  \|\   ____\\___   ___\\  ___ \    |\  \  /  /|\___   ___\ 
-\ \  \|\ /\ \  \ \  \___\|___ \  \_\ \   __/|   \ \  \/  / ||___ \  \_| 
- \ \   __  \ \  \ \  \  ___  \ \  \ \ \  \_|/__  \ \    / /     \ \  \  
-  \ \  \|\  \ \  \ \  \|\  \  \ \  \ \ \  \_|\ \  /     \/       \ \  \ 
-   \ \_______\ \__\ \_______\  \ \__\ \ \_______\/  /\   \        \ \__\
-    \|_______|\|__|\|_______|   \|__|  \|_______/__/ /\ __\        \|__|
+sh modules/Example/bin/mod.sh
+		
+#cyan="python  /data/data/com.termux/files/usr/share/cyan.py"
+#$cyan
+#echo "
+ #________  ___  ________ _________  _______      ___    ___ _________   
+#|\   __  \|\  \|\   ____\\___   ___\\  ___ \    |\  \  /  /|\___   ___\ 
+#\ \  \|\ /\ \  \ \  \___\|___ \  \_\ \   __/|   \ \  \/  / ||___ \  \_| 
+# \ \   __  \ \  \ \  \  ___  \ \  \ \ \  \_|/__  \ \    / /     \ \  \  
+#  \ \  \|\  \ \  \ \  \|\  \  \ \  \ \ \  \_|\ \  /     \/       \ \  \ 
+ #  \ \_______\ \__\ \_______\  \ \__\ \ \_______\/  /\   \        \ \__\
+ #   \|_______|\|__|\|_______|   \|__|  \|_______/__/ /\ __\        \|__|
                                                 |__|/ \|__|             
    
-" | lolcat
+#"  lolcat
 
-echo -e
-sleep 3
-python red.py
-echo ""
-echo "Installing Packages"
-pip install colorama
-apt install figlet
+#echo -e
+#sleep 3
+#python red.py
+#echo ""
+#echo "Installing Packages"
+#pip install colorama
+#apt install figlet
 
 
-echo "I| Now type text and press enter to get big ascii version" | lolcat
-python /data/data/com.termux/files/usr/share/red.py
-echo "Notice| You have 10 tries"
-$cyan
-echo "text>>"
-read text
-figlet $text
-read text
-figlet $text
-read text
-figlet $text
-read text
-figlet $text
-read text
-figlet $text
-read text
-figlet $text
-read text
-figlet $text
-read text
-figlet $text
-read text
-figlet $text
-read text
-figlet $text
+#echo "I| Now type text and press enter to get big ascii version" | lolcat
+#python /data/data/com.termux/files/usr/share/red.py
+#echo "Notice| You have 10 tries"
+#$cyan
+#echo "text>>"
+#read text
+#figlet $text
+#read text
+#figlet $text
+#read text
+#figlet $text
+#read text
+#figlet $text
+#read text
+
